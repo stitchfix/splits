85f5f2792cb7a6cb124b050a94b47c614da2e326
Jeff Magnusson <magnussj@gmail.com>
2014-09-09 17:34:32 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='splits',
-    version='0.0.4',
+    version='0.0.5',
     author='Thomas Millar, Jeff Magnusson',
     author_email='millar.thomas@gmail.com, magnussj@gmail.com',
     license='MIT',
