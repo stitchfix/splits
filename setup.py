68ee748f38110f414ab1d495e7355e73ac8f66c6
Thomas <tmillar@stitchfix.com>
2018-03-01 13:37:09 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,20 +2,16 @@ from distutils.core import setup
 
 setup(
     name='splits',
-    version='0.1.1',
+    version='0.1.2',
     author='Thomas Millar, Jeff Magnusson',
     author_email='millar.thomas@gmail.com, magnussj@gmail.com',
     license='MIT',
     description='A library for dealing with splittable files',
-    packages = ['splits'],
+    packages=['splits'],
     long_description='Library for dealing with splittable files',
     url='https://github.com/stitchfix/splits',
     keywords=['split', 'multifile', 'splittable'],
     classifiers=[
         'Intended Audience :: Developers',
     ],
-    install_requires=[
-        'boto',
-        'nose'
-    ]
-)
+    install_requires=['boto', 'nose'])
