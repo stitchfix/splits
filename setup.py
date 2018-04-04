a6c3782d6c60f3660b7978d7311335e093c9a179
Jeff Magnusson <magnussj@gmail.com>
2018-04-04 12:32:26 -0700

    bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setup(
     name='splits',
-    version='0.1.6',
+    version='0.1.7',
     author='Thomas Millar, Jeff Magnusson',
     author_email='millar.thomas@gmail.com, magnussj@gmail.com',
     license='MIT',
